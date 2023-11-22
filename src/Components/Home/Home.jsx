f20d721fc5c165ca6994d6a48cde751150be99b0
junayet <muhammadjunayetmaruf@gmail.com>
2023-11-22 11:04:10 +0600

    curl-canvas-frontend has started

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-3xl font-bold text-center">home</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/MyRouts/MyRouts.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Components/MainLayout/MainLayout";
+import Home from "../Components/Home/Home";
+
+const MyRouts = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      children: [
+        {
+          path:'/',
+          element:<Home></Home>
+        }
+      ]
+    },
+  ]);
+
+  
+  
+  export default MyRouts;
